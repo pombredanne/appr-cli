49113f686e5aac5e86399142b3f33e4fafa823bd
ant31 <2t.antoine@gmail.com>
2016-11-25 16:50:27 +0100

    Add manifests type in list api

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
 >>>>>>> d6f1c84... New data models + tests
 setup(
     name='cnr-server',
-    version='0.2.1',
+    version='0.2.2',
     description="cloud-native app registry server",
     long_description=readme,
     author="Antoine Legrand",
