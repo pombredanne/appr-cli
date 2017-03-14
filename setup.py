1778c48307fef0664c6dbd6b4c34319a2d737ba7
Antoine Legrand <antoine.legrand@coreos.com>
2017-03-14 18:16:17 +0100

    Add tests

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
 >>>>>>> d6f1c84... New data models + tests
 setup(
     name='cnr-server',
-    version='0.2.6-1',
+    version='0.2.7-1',
     description="cloud-native app registry server",
     long_description=readme,
     author="Antoine Legrand",
